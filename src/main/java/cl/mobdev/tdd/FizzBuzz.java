5bfae938e82b13eb92c6c6fdbb7ea99e704aae6c
spring-backend-mobdev <enieto@mobdev.cl>
2022-02-10 11:55:52 -0300

    first commit

---
+++ src/main/java/cl/mobdev/tdd/FizzBuzz.java
@@ -0,0 +1,23 @@
+package cl.mobdev.tdd;
+
+public class FizzBuzz {
+    // Método que recibirá un input (tipo int) y retornará un String de su mismo valor
+    // pero si este input es un múltiplo de 3 retornará Fizz
+    // y si este input es múltiplo de 5 retornará Buzz
+    // en caso de que el input sea múltiplo de 3 y 5 retornará FizzBuzz
+
+    public String convert(int input) {
+        if (input < 1) {
+            return "0";
+        }
+
+        if (input % 3 == 0 && input % 5 == 0) {
+            return "FizzBuzz";
+        } else if (input % 3 == 0) {
+            return "Fizz";
+        } else if (input % 5 == 0) {
+            return "Buzz";
+        }
+        return String.valueOf(input);
+    }
+}
---
+++ src/test/java/cl/mobdev/tdd/FizzBuzzTest.java
@@ -0,0 +1,47 @@
+package cl.mobdev.tdd;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    @DisplayName("")
+    void should_return_correct_output_for_input() {
+        String expected = "1";
+        String response = fizzBuzz.convert(1);
+        assertEquals(expected, response);
+    }
+
+    @Test
+    void should_return_fizz_when_input_is_multiple_by_3() {
+        String expected = "Fizz";
+        String response = fizzBuzz.convert(3);
+        assertEquals(expected, response);
+    }
+
+    @Test
+    void sholud_return_buzz_when_input_is_multiple_by_5() {
+        String expected = "Buzz";
+        String response = fizzBuzz.convert(5);
+        assertEquals(expected, response);
+    }
+
+    @Test
+    void should_return_fizzbuzz_when_input_is_multiple_by_3_and_5() {
+        String expected = "FizzBuzz";
+        String response = fizzBuzz.convert(15);
+        assertEquals(expected, response);
+    }
+}
