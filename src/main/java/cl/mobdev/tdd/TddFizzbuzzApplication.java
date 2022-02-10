0cf12b3a4666b51754855e6171d5b5f503801ba1
spring-backend-mobdev <enieto@mobdev.cl>
2022-02-10 10:53:12 -0300

    Initial commit

---
+++ src/main/java/cl/mobdev/tdd/TddFizzbuzzApplication.java
@@ -0,0 +1,13 @@
+package cl.mobdev.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddFizzbuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddFizzbuzzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/mobdev/tdd/TddFizzbuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.mobdev.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddFizzbuzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
